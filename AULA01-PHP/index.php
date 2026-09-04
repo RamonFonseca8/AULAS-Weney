7ef639c14804dd975e4a6c485c7ce1e365568346
Ramon Fonseca Lourenço <ramonlou08@gmail.com>
2026-09-03 22:01:21 -0300

    Add files via upload

---
+++ AULA01-PHP/index.php
@@ -0,0 +1,23 @@
+<?php
+
+    echo "Olá, Mundo!<br><br>";
+    $num1 = 4;
+    $num2 = 2;
+    $num3 = 6;
+    $nome = "Ramon";
+    $sub =  $num1 - $num2;
+    $mult =  $num1 * $num2;
+    $div =  $num1 / $num2;
+    $soma = $num1 + $num2;
+    $delta = $num2 * $num2 - 4 * $num1 * $num3;
+ 
+
+    echo "A soma de $num1 + $num2 é igual a $soma.<br><br>";
+    echo "A subtração de $num1 - $num2 é igual $sub.<br><br>";
+    echo "A multiplicação de $num1 x $num2 é igual a $mult.<br><br>";
+    echo "A divisão de $num1 / $num2 é igual a $div.<br><br>";
+    echo "A equação de $num2 ² - 4 * $num1 * $num3 é igual a $delta.";
+
+
+
+?>
---
+++ AULA01-PHP/media.php
@@ -0,0 +1,10 @@
+<?php
+
+    $num1 = 4;
+    $num2 = 2;
+    $num3 = 6;
+    $media = ($num1 + $num2 + $num3)/3;
+
+    echo "A média dos números $num1, $num2 e $num3 é $media";
+
+?>
